db98c9dbcf7a5c1b3a78b8043c6541aea14551c3
Demehin Ibukunoluwa George <ibk2k7@gmail.com>
2022-01-06 15:33:00 +0100

    feat: added the question form and its logic

---
+++ pages/triviagame/addquestion.js
@@ -0,0 +1,21 @@
+import { Heading } from "@chakra-ui/react";
+import React from "react";
+import Layout from "../../components/layout/layout";
+import NavHeader from "../../components/nav/header.component";
+import OneQuestion from "../../components/triviagames/questions.component";
+
+const TrivisGamesPage = () => {
+  return (
+    <Layout name="trivas" desc="I-Predict Trivas Game">
+      <NavHeader />
+      <div className="max-w-sm mx-auto">
+        <div className="text text-center my-5">
+          <Heading>Add a question</Heading>
+        </div>
+        <OneQuestion />
+      </div>
+    </Layout>
+  );
+};
+
+export default TrivisGamesPage;
